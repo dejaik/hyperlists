e35b72cf8b20db8374b5f9de73dcff9c516683fc
dejaik <info@imiiacreative.com>
2018-07-13 19:12:11 -0400

    Initial Commit

--- docs/src.bce157e6.js
+++ docs/src.bce157e6.js
@@ -719,7 +719,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '58488' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '58695' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
